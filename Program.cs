a5e455259d95b57352601b634d5c669976dd782f
Laminfal29 <158592165+Laminfal29@users.noreply.github.com>
2024-02-12 16:23:32 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,3 +6,5 @@ console.writeline("Je m' appelle Lamine Fall");
 
 console.writeline("Je suis apprenant a l' isep diamniadio");
 
+console.writeline("Je suis dans la filiere d 'administration de base de donnees");
+
